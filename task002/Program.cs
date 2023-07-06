6c98b7e766c756ef325a141357c7341f671e00fb
Roman & Anastasiya <roma.gostev2017@yandex.ru>
2023-07-06 15:09:27 +0700

    task002

---
+++ task002/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите первое число: number1 = ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: number2 = ");
+int number2 = int.Parse(Console.ReadLine());
+if (number1 > number2)
+{
+    Console.WriteLine($"Большее число между числами {number1} и {number2} является " + number1, number1, number2+".");
+    Console.Write($"Меньшее, соответственно, " + number2);
+}
+else
+{
+    Console.WriteLine($"Большее число между числами {number1} и {number2} является " + number2, number1, number2+".");
+    Console.Write($"Меньшее, соответственно, " + number1);
+}
