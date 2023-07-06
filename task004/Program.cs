e0bc5dee69630f00b8e2ea4c8852da23ffffb328
Roman & Anastasiya <roma.gostev2017@yandex.ru>
2023-07-06 15:24:54 +0700

    task004

---
+++ task004/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Write("Введите первое число: number1 = ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: number2 = ");
+int number2 = int.Parse(Console.ReadLine());
+Console.Write("Введите первое число: number3 = ");
+int number3 = int.Parse(Console.ReadLine());
+int max = 0;
+if (number1<number2)
+{
+    max=number2;
+}
+else
+{
+    if (number2<number3)
+    {
+        max=number3;
+    }
+    else
+    {
+        if (number3<number1)
+        {
+            max=number1;
+        }
+    }
+}
+Console.WriteLine($"Максимальное число среди чисел {number1}, {number2}, {number3} равно "+max);
