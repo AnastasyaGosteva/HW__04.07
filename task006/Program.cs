3076393f01e0497174e86f5908f68677fc83a81d
Roman & Anastasiya <roma.gostev2017@yandex.ru>
2023-07-06 15:33:32 +0700

    task006/

---
+++ task006/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число для проверки на четность: number = ");
+int number = int.Parse(Console.ReadLine());
+if (number%2==0)
+{
+    Console.Write($"Число {number} является четным.");
+}
+else
+{
+    Console.Write($"Число {number} неявляется четным.");
+}
