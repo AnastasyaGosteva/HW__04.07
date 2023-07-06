5d7b31566441a4897cf085b84c195d7822db3ab6
Roman & Anastasiya <roma.gostev2017@yandex.ru>
2023-07-06 17:01:42 +0700

    Доработала task002

--- task002/Program.cs
+++ task002/Program.cs
@@ -5,10 +5,10 @@ int number2 = int.Parse(Console.ReadLine());
 if (number1 > number2)
 {
     Console.WriteLine($"Большее число между числами {number1} и {number2} является " + number1, number1, number2+".");
-    Console.Write($"Меньшее, соответственно, " + number2);
+    Console.Write($"Меньшее, соответственно, " + number2+".");
 }
 else
 {
     Console.WriteLine($"Большее число между числами {number1} и {number2} является " + number2, number1, number2+".");
-    Console.Write($"Меньшее, соответственно, " + number1);
+    Console.Write($"Меньшее, соответственно, " + number1+".");
 }
