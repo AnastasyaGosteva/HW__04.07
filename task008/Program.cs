dd8e8eecadfed9b2b2e147c7340f5f6b39c55e54
Roman & Anastasiya <roma.gostev2017@yandex.ru>
2023-07-06 17:30:59 +0700

    Почистила кэш

--- task008/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿Console.Write("Введите число: number = ");
-int number = int.Parse(Console.ReadLine());
-for (int i=1; i<=number; i++)
-{
-    if (i%2==0)
-    {
-        Console.Write(i+" ");
-    }
-}
